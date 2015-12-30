0700c62adec6a59853255776ecb0e008058c5f12
Adam Carlsten <addecrea3@hotmail.com>
2015-12-30 06:08:07 +0100

    started exercise 3

---
+++ substrings.rb
@@ -0,0 +1,8 @@
+def substrings(text, dictionary)
+  x = text.split('')
+  p x
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Hello", dictionary)
